8ce2a6c4700d97e8d692638c5b863e6bd1ff5657
Leonardo Wistuba <leonardo.wistuba@teampicnic.com>
2020-06-25 21:53:52 +0200

    Enables strict mode

--- src/workers/next-release-guesser.ts
+++ src/workers/next-release-guesser.ts
@@ -21,9 +21,9 @@ export class GithubNextReleaseGuesser implements NextReleaseGuesser {
   guess(): Observable<string> {
     // https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
     const semverRegex = /w*(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
-    const semverFromString = (value: string): string => {
+    const semverFromString = (value: string): string | null => {
       return value.match(semverRegex) || [].length > 0
-        ? value.match(semverRegex)[0]
+        ? value.match(semverRegex)![0]
         : null;
     };
 
@@ -39,10 +39,23 @@ export class GithubNextReleaseGuesser implements NextReleaseGuesser {
       .pipe(map((response) => response.data.map((pull) => pull.title)))
       .pipe(
         map((titles) =>
-          titles.map(semverFromString).filter((semver) => semver != null)
+          titles.map(semverFromString)
+            .filter((semver) => semver != null)
+            .filter((x): x is string => x !== null)
         )
       )
-      .pipe(map((semvers) => (semvers.length > 0 ? semvers[0] : '1.0.0')))
-      .pipe(map((latestSemver) => semver.inc(latestSemver, 'patch')));
+      .pipe(map(semvers => {
+        if (semvers.length > 0) {
+          return semvers[0]
+        }
+        throw new Error("Unable to find semvers in the PR titles")
+      }))
+      .pipe(map((latestSemver) => { 
+        const increasedSemver = semver.inc(latestSemver, 'patch')
+        if (increasedSemver == null) {
+          throw new Error("Unable to increase semver")
+        }
+        return increasedSemver!
+      }));
   }
 }
