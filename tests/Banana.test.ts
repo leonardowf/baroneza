4a36099f97a1a5412a9d80887e7213c5f3bc55cf
Leonardo Wistuba <leonardo.wistuba@teampicnic.com>
2020-07-18 19:20:13 +0200

    Implements base test

--- tests/Banana.test.ts
+++ tests/Banana.test.ts
@@ -1,5 +1,13 @@
-describe("oi", () => {
-    it("ola", () => {
-        expect(7).toBe(7);  
+import { TestScheduler } from 'rxjs/testing';
+import { Observable, from } from 'rxjs';
+
+describe("simple suite", () => {
+    it("simple scenario", done => {
+        from("simple stream").subscribe({
+            next: (x) => {
+                // Just a simple template to test streams
+            },
+            complete: done
+        })
     })
 })
