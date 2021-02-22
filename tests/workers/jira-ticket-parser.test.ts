8aa6b66bd5fab87e889f0797fd3eb546fd55159f
leonardowistuba <leonardowistuba@gmail.com>
2021-02-22 21:41:13 +0100

    Support multiple tickets per commit

--- tests/workers/jira-ticket-parser.test.ts
+++ tests/workers/jira-ticket-parser.test.ts
@@ -7,10 +7,18 @@ describe('The jira ticket parser', () => {
   });
 
   it('works with number tags', () => {
-    expect(sut.parse(['[PSF-123] Bla Bla'])).toStrictEqual(['PSF-123']);
+    expect(sut.parse(['[PSF-123][PSF-456] [PSF-78910] Bla Bla'])).toStrictEqual(['PSF-123', 'PSF-456', 'PSF-78910']);
   });
 
   it('does not work with number tags', () => {
     expect(sut.parse(['[PSF-abc] Bla Bla'])).toStrictEqual([]);
+  });
+
+  it('does not duplicate tickets', () => {
+    expect(sut.parse(['[PSF-13][PSF-13] Bla Bla'])).toStrictEqual(['PSF-13']);
+  });
+
+  it('works without brackets', () => {
+    expect(sut.parse(['PSF-123 PSF-123 Bla Bla'])).toStrictEqual(['PSF-123']);
   });
 });
