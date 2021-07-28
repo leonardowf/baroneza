410de975e1b7efbd718a34983b2e2dce66a6f4e0
Lint Action <lint-action@samuelmeuli.com>
2021-07-28 20:38:42 +0000

    Fix code style issues with ESLint

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-    transform: {'^.+\\.ts?$': 'ts-jest'},
-    testEnvironment: 'node',
-    testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    collectCoverage: true,
-    collectCoverageFrom: [
-        'src/**/*.{js,ts}',
-        '!src/typings/**/*', 
-        '!src/services/*',
-        '!src/configs.ts',
-        '!src/keys.ts',
-        '!src/process-env.ts',
-        '!src/dependencies.ts',
-        '!src/app.ts'
-    ]
+  transform: { '^.+\\.ts?$': 'ts-jest' },
+  testEnvironment: 'node',
+  testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,ts}',
+    '!src/typings/**/*',
+    '!src/services/*',
+    '!src/configs.ts',
+    '!src/keys.ts',
+    '!src/process-env.ts',
+    '!src/dependencies.ts',
+    '!src/app.ts'
+  ]
 };
